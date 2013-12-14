6e0d4e66099897ebc5674d9bb0f473c26404216b
Alessandro Ghedini <alessandro@ghedini.me>
2013-12-14 15:06:19 +0100

    user: do not call getpwnam() if user == 'root'

--- src/user.c
+++ src/user.c
@@ -46,19 +46,30 @@ void map_user_to_user(uid_t uid, gid_t gid, char *user) {
 	_close_ int uid_map_fd = -1;
 	_close_ int gid_map_fd = -1;
 
-	struct passwd *pwd;
-
 	_free_ char *uid_map = NULL;
 	_free_ char *gid_map = NULL;
 
-	errno = 0;
-	pwd = getpwnam(user);
-	if (pwd == NULL) {
-		if (errno) sysf_printf("getpwnam()");
-		else       fail_printf("Invalid user '%s'", user);
+	uid_t pw_uid;
+	uid_t pw_gid;
+
+	struct passwd *pwd;
+
+	if (strncmp(user, "root", 5) == 0) {
+		pw_uid = 0;
+		pw_gid = 0;
+	} else {
+		errno = 0;
+		pwd = getpwnam(user);
+		if (pwd == NULL) {
+			if (errno) sysf_printf("getpwnam()");
+			else       fail_printf("Invalid user '%s'", user);
+		}
+
+		pw_uid = pwd -> pw_uid;
+		pw_gid = pwd -> pw_gid;
 	}
 
-	rc = asprintf(&uid_map, "%d %d 1", pwd -> pw_uid, uid);
+	rc = asprintf(&uid_map, "%d %d 1", pw_uid, uid);
 	if (rc < 0) fail_printf("OOM");
 
 	uid_map_fd = open("/proc/self/uid_map", O_RDWR);
@@ -67,7 +78,7 @@ void map_user_to_user(uid_t uid, gid_t gid, char *user) {
 	rc = write(uid_map_fd, uid_map, strlen(uid_map));
 	if (rc < 0) sysf_printf("write(uid_map)");
 
-	rc = asprintf(&gid_map, "%d %d 1", pwd -> pw_gid, gid);
+	rc = asprintf(&gid_map, "%d %d 1", pw_gid, gid);
 	if (rc < 0) fail_printf("OOM");
 
 	gid_map_fd = open("/proc/self/gid_map", O_RDWR);
@@ -80,21 +91,32 @@ void map_user_to_user(uid_t uid, gid_t gid, char *user) {
 void do_user(char *user) {
 	int rc;
 
+	uid_t pw_uid;
+	uid_t pw_gid;
+
 	struct passwd *pwd;
 
-	errno = 0;
-	pwd = getpwnam(user);
-	if (pwd == NULL) {
-		if (errno) sysf_printf("getpwnam()");
-		else       fail_printf("Invalid user '%s'", user);
+	if (strncmp(user, "root", 5) == 0) {
+		pw_uid = 0;
+		pw_gid = 0;
+	} else {
+		errno = 0;
+		pwd = getpwnam(user);
+		if (pwd == NULL) {
+			if (errno) sysf_printf("getpwnam()");
+			else       fail_printf("Invalid user '%s'", user);
+		}
+
+		pw_uid = pwd -> pw_uid;
+		pw_gid = pwd -> pw_gid;
 	}
 
-	rc = initgroups(user, pwd -> pw_gid);
+	rc = initgroups(user, pw_gid);
 	if (rc < 0) sysf_printf("initgroups()");
 
-	rc = setresgid(pwd -> pw_gid, pwd -> pw_gid, pwd -> pw_gid);
+	rc = setresgid(pw_gid, pw_gid, pw_gid);
 	if (rc < 0) sysf_printf("setresgid()");
 
-	rc = setresuid(pwd -> pw_uid, pwd -> pw_uid, pwd -> pw_uid);
+	rc = setresuid(pw_uid, pw_uid, pw_uid);
 	if (rc < 0) sysf_printf("setresuid()");
 }
