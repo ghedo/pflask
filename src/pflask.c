721fe434024563914fc16ddc5b70d8c57d90db9b
Alessandro Ghedini <alessandro@ghedini.me>
2015-06-21 15:31:06 +0200

    pflask: use child pid to register machine
    This way stuff like "machinectl login" work, as long as systemd (and dbus)
    is run inside the container.
    
    However this breaks "machinectl poweroff" if systemd is not PID 1 (the
    "terminate" command must be used instead).

--- src/pflask.c
+++ src/pflask.c
@@ -352,7 +352,7 @@ int main(int argc, char *argv[]) {
 	}
 
 #ifdef HAVE_DBUS
-	register_machine(getpid(), dest != NULL ? dest : "");
+	register_machine(pid, dest != NULL ? dest : "");
 #endif
 
 	do_netif(pid);
